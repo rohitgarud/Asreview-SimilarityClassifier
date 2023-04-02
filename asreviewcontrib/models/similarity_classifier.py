280054753ae3d1c109a8db83c993de6caeb47eb3
rohitgarud <rohit.garuda1992@gmail.com>
2023-04-02 13:10:04 +0530

    Add Euclidean distance similarity metric

--- asreviewcontrib/models/similarity_classifier.py
+++ asreviewcontrib/models/similarity_classifier.py
@@ -5,6 +5,18 @@ from sklearn.metrics import pairwise_distances
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+def _l2_norm(x):
+    return np.sqrt(np.sum(x**2))
+
+
+def _div_norm(x):
+    norm_value = _l2_norm(x)
+    if norm_value > 0:
+        return x * (1.0 / norm_value)
+    else:
+        return x
+
+
 class SimilarClassifier:
     def __init__(
         self,
@@ -26,6 +38,8 @@ class SimilarClassifier:
             # Taking mean of features of relevant records
             self.relevant = X[y == 1, :]
             self.resultant = (sum(self.relevant) / len(self.relevant)).reshape(1, -1)
+            if self.similarity_metric == "euclidean_dist":
+                self.resultant = _div_norm(self.resultant)
 
     def predict_proba(self, X):
         """Get the inclusion probability for each sample."""
@@ -40,5 +54,8 @@ class SimilarClassifier:
         if self.similarity_metric == "dot_product":
             sim = np.array([np.dot(self.resultant, X[i, :]) for i in range(len(X))])
             sim = 1 - sim
+        if self.similarity_metric == "euclidean_dist":
+            X = np.array([_div_norm(np.array(X[i, :])) for i in range(len(X))])
+            sim = pairwise_distances(X, self.resultant, metric="euclidean")
 
         return sim
